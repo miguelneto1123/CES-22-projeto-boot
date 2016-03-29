e080ee1ad657049eb0fa16a0dc58b7453a0874c1
miguel <miguel@miguel-VPCEA36FM.(none)>
2016-03-29 16:17:28 -0300

    Colocando a área de texto

--- Notepad/src/Notepad.java
+++ Notepad/src/Notepad.java
@@ -1,9 +1,20 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-import javax.swing.JFrame;
+import javax.swing.*; // para o design do JFrame principal
+import java.awt.*; // para fazer a GUI
+import java.awt.event.*; // para lidar com eventos
+import java.util.Scanner; // para ler de arquivos
+import java.io.*; // para escrever em arquivos
 
 public class Notepad extends JFrame implements ActionListener {
+	private TextArea textArea = new TextArea("", 0,0,
+			TextArea.SCROLLBARS_VERTICAL_ONLY);
+	/*
+	 * Aqui é a parte principal do aplicativo. O primeiro argumento do
+	 * construtor é uma string vazia, que é o "texto inicial" que
+	 * aparece quando se abre o programa. Se fosse passado "CES-22", ia
+	 * abrir a tela com "CES-22". O 0,0 é o tamanho da área de texto. Não
+	 * importa definir porque vai ficar na tela inteira. A flag final
+	 * serve para fazer wrapping de palavras
+	 * */
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
